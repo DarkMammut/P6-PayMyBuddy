7ef7505a154d9c98bb7e7df55891d8b89373c799
Sébastien BELLANGER <seb.bellanger@gmail.com>
2024-08-05 07:34:35 +0200

    corrections bugs

--- src/test/java/com/paymybuddy/moneytransfer/PayMyBuddyApplicationTests.java
+++ src/test/java/com/paymybuddy/moneytransfer/PayMyBuddyApplicationTests.java
@@ -1,13 +1,7 @@
 package com.paymybuddy.moneytransfer;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PayMyBuddyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
